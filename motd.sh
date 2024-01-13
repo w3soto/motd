093d08cee419ccd7a8c64c3262aa2e50fbfd015e
w3soto <soto.ms@gmail.com>
2024-01-13 09:22:41 +0100

    Add arguments handling

--- motd.sh
+++ motd.sh
@@ -2,20 +2,37 @@
 
 ###############################################################################
 # Message of the day
-# Usage: modt.sh [console|notify] [index]
-# Author: Soto
+# Author: Soto (soto.ms@gmail.com)
 ###############################################################################
 
 DIR=`dirname "$0"`
-MODE="$1"
-INDEX="$2"
-
+OUTPUT="console"
+INDEX=
+SOURCE_FILE="motd.txt"
 QUOTE=""
 AUTHOR=""
 
+###############################################################################
+# FUNCTIONS
+###############################################################################
+function help() {
+cat <<EOF
+
+    Usage:
+        modt.sh [OPTION...]
+
+    Options:
+        -o, --output=OUTPUT         Specifies the output. Available values are "console" (default) or "notify".
+        -i, --index=INDEX           Specifies the index line of source file. If not set, random line will be used.
+        -s, --source=SOURCE_FILE    Specifies the source file. By default "motd.txt".
+        -h, --help                  Show options
+
+EOF
+}
+
 function notify_print() {
 	next_motd
-	$DIR/notify-send.sh --icon=$HOME/.face --urgency=critical --expire-time=10000 "Welcome $USER" "\n$QUOTE\n\n$AUTHOR\n" -o "Next:$DIR/motd.sh notify"
+	$DIR/notify-send.sh --icon=$HOME/.face --urgency=critical "Welcome $USER" "\n$QUOTE\n\n$AUTHOR\n" -o "Next:$DIR/motd.sh -o notify"
 }
 
 function console_print() {
@@ -28,18 +45,51 @@ function console_print() {
 function next_motd() {
 	# local version
     if [[ $INDEX != "" ]]; then
-        line=$(sed "${INDEX}q;d" $DIR/motd.txt)
+        line=$(sed "${INDEX}q;d" $DIR/$SOURCE_FILE)
     else
-        line=$(shuf -n 1 $DIR/motd.txt)
+        line=$(shuf -n 1 $DIR/$SOURCE_FILE)
     fi
     QUOTE=$(echo $line | sed -r 's/(\-\-.+)//g')
     AUTHOR=$(echo $line | sed -r 's/(".+" )//g')
 }
 
-if [[ $MODE == "notify" ]]; then
+
+###############################################################################
+# PROCESS ARGUMENTS
+###############################################################################
+while (( $# > 0 )) ; do
+    case "$1" in
+        -h|--help)
+            help
+            exit 0
+            ;;
+        -v|--version)
+            echo "${0##*/} $VERSION"
+            exit 0
+            ;;
+        -o|--output|--output=*)
+            [[ "$1" = --output=* ]] && output="${1#*=}" || { shift; output="$1"; }
+            OUTPUT="$output"
+            ;;
+        -i|--index|--index=*)
+            [[ "$1" = --index=* ]] && index="${1#*=}" || { shift; index="$1"; }
+            INDEX="$index"
+            ;;
+        -s|--source|--source=*)
+            [[ "$1" = --source=* ]] && source="${1#*=}" || { shift; source="$1"; }
+            SOURCE_FILE="$source"
+            ;;
+    esac
+    shift
+done;
+
+###############################################################################
+# RUN
+###############################################################################
+if [[ $OUTPUT == "notify" ]]; then
     notify_print
 else
 	console_print
 fi
 
-
+exit 0	
